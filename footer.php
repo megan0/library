1c544fd9a41a931b07e7d9c8e3278de9d4955c51
megan0 <51860978+megan0@users.noreply.github.com>
2020-08-22 14:24:32 +0200

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,34 @@
+<footer >
+<nav class="navbar navbar-expand-sm bg-light navbar-light">
+   <div class="container text-center">
+   <div class="row">
+   
+      <div class="col-xs-4 ">
+         <h3 class='h3'>Library</h3>
+         <ul class="list-unstyled">
+         <li><a href="#">Kontakt</a></li>
+         <li><a href="#">Rreth nesh</a></li>
+         <li><a href="#">Dyqanet</a></li>
+         <li><a href="#">Vende pune</a></li></ul></div>
+      </div>
+
+         <div class="col-xs-4">
+            <h3 class='h3'>Informacion</h3><ul class="list-unstyled">
+            <li><a href="#">Mënyrat e Pagesës</a></li>
+            <li><a href="#">Mënyrat e Dërgesës</a></li>
+            <li><a href="#">Kushtet e përdorimit</a></li>
+            <li><a href="#">Ndihmë/FAQ</a></li>
+         </ul>
+         </div>
+
+         <div class="col-xs-4">
+            <h3 class='h3'>Kontakt</h3>
+            <ul class="list-unstyled">
+            <li><a>04 231 9002</a></li>
+            <li><p><b>Kujdesi ndaj klientit</b> <br> E Hënë - E Premte, ora 09.00 - 17.00 <br> E Shtunë, ora 09.00 - 13.00 <br></p></li>
+            </ul>
+         </div>
+   </div>
+   </div>
+</nav>
+</footer>
---
+++ header.php
@@ -0,0 +1,15 @@
+<!--header-->
+
+<div>
+   <nav class="navbar navbar-expand-sm bg-light navbar-light">
+       <a class="navbar-brand" href="#"><img src='foto/logo.jpg'  width=180 height=80></a>
+       <ul class="navbar-nav collapse navbar-collapse justify-content-end">
+            <li class="nav-item">
+            <a class="nav-link nav1" href="#">SignUp</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link nav1" href="#">LogIn</a>
+            </li>
+        </ul>
+   </nav>
+</div>
