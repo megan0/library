b3d56724c98c025022076c2711e9ef7fc505a290
Megan <megan.ismaili@fshnstudent.info>
2020-09-02 15:39:24 +0200

    rregullime admin 1

--- admin.php
+++ admin.php
@@ -1,9 +1,22 @@
-<<!doctype html>
+<!doctype html>
 <html lang="en">
   <head>
     <title>Administrator</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="project.css">
+    <style>
+      #but{
+        font-family: 'Ubuntu', sans-serif;
+        font-style: italic;
+        font-size: 20px;
+        padding: 40px ;
+        background-color: rgb(233, 228, 224);
+        color:gray;
+
+      }
+    </style>
+
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   </head>
@@ -21,7 +34,45 @@
 
 
     ?>
-    
+
+<br>
+<br>
+    <div class='container'>
+      <div class='row'>
+        <div class='col-md-2'>
+          <img src='foto/admin.png' alt='foto profili' width=100 height=100>
+        </div>
+
+        <div class='col-md-4 '>
+          <?php
+            
+            $id=$_SESSION["id"];
+            $sql = "SELECT emer,mbiemer FROM perdorues WHERE id =$id";
+            $result = $conn->query($sql);
+            if ($result->num_rows == 1){
+              $row = $result->fetch_assoc();
+              echo "<h2 class='h2'>".$row["emer"]." ".$row["mbiemer"]."</h2>";
+            } 
+
+          ?>
+        </div>
+      </div>
+      <br><br>
+      <div class='row'>
+          <div class='col-md-4 '>
+            <a href="menaxho_liber.php" class=" btn btn-secondary " id="but">Menaxho Liber</a>
+          </div>
+          <div class='col-md-4 '>
+            <a href="menaxho_autor.php" class="btn btn-secondary" id="but">Menaxho Autor</a>
+          </div>
+          <div class='col-md-4 '>
+            <a href="ulje.php" class="btn btn-secondary" id="but">Apliko Ulje</a>
+          </div>
+
+      </div>
+    </div>
+ <br>
+ <br>   
 
 
     <?php
